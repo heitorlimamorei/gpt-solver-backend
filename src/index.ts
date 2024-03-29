45f4a31ca1ad4b1de9bc4434974f9ef8a7f4ccb1
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-03-28 22:35:53 -0300

    chore: replacing the limit of body parser

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
 app.use("/v1/user", userRouter);
 app.use("/v1/chat", chatRouter);
 
